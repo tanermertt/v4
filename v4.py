9c0bc4f0729b34208d0052fc815d8bd05681dd03
tanermertt <tmert@yandex.com>
2025-04-08 11:00:49 +0300

    Update v4.py

--- v4.py
+++ v4.py
@@ -212,5 +212,7 @@ if st.button("Hesapla"):
         st.subheader("Net Maaş")
         st.write(f"Bankaya Yatan Net Maaş: {sonuclar['Net Maaş']:.2f} TL")  
         
-          
+  import pandas as pd
+    df_yardimlar = pd.DataFrame.from_dict(sonuclar["Yardımlar Özeti"], orient='index', columns=["Tutar (TL)"])
+    st.dataframe(df_yardimlar.style.format("{:.2f}"))        
       
